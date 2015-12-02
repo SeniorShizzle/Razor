36194268c6333c19960ae5b583868980d91f8655
SeniorShizzle <SeniorShizzle@gmail.com>
2015-12-02 15:58:17 -0800

    Initial Commit
    Woo

---
+++ src/ParallelEdgeCounter.java
@@ -0,0 +1,21 @@
+
+public class ParallelEdgeCounter {
+
+	/**
+	 * 
+	 * @param g graph to process
+	 */
+	public ParallelEdgeCounter(Graph g) {
+		
+		// TODO your implementation here
+	}
+	
+	/**
+	 * 
+	 * @return the total # of parallel edges in the graph
+	 */
+	public int size() { 
+		// TODO your implementation here
+		return 0;
+	}
+}
